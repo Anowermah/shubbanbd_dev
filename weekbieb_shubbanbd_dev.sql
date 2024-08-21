59fbbfd0491fdd88b8115afbb2521d9170608d19
Anower <anowar266927@gmail.com>
2024-08-21 21:36:38 +0600

    website is run dev shubbanbd

--- weekbieb_shubbanbd_dev.sql
+++ weekbieb_shubbanbd_dev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 21, 2024 at 08:56 AM
+-- Generation Time: Aug 21, 2024 at 02:16 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
